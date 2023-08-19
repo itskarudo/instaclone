bb43daec9b0cfe60c4d1e6959563982ee62d2799
itskarudo <itskarudo@protonmail.com>
2023-08-19 16:21:50 +0100

    Client: Add show/hide password buttons to login and register forms

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,6 +7,7 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import getUrqlClient from "@/utils/getUrqlClient";
 import { graphql } from "@/generated";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const pacifico = Pacifico({ weight: "400", subsets: ["latin"] });
 
@@ -19,6 +20,8 @@ const Login = () => {
   const router = useRouter();
   const client = getUrqlClient();
 
+  const [passwordHidden, setPasswordHidden] = useState(true);
+
   const query = graphql(`
     mutation Login($usernameOrEmail: String!, $password: String!) {
       login(usernameOrEmail: $usernameOrEmail, password: $password)
@@ -27,11 +30,14 @@ const Login = () => {
 
   const {
     register,
+    watch,
     handleSubmit,
     formState: { isValid },
   } = useForm<Inputs>({
     mode: "onChange",
   });
+
+  const password = watch("password");
 
   const onSubmit: SubmitHandler<Inputs> = async (inputs) => {
     const { data, error } = await client.mutation(query, {
@@ -60,24 +66,36 @@ const Login = () => {
             Instaclone
           </h1>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <input
-              className="block border-[1px] border-slate-300 border-solid p-2 text-xs w-full mb-3 focus:border-slate-400 focus:outline-none rounded"
-              type="text"
-              placeholder="Username or email"
-              {...register("usernameOrEmail", {
-                required: true,
-                minLength: 4,
-              })}
-            />
-            <input
-              className="block border-[1px] border-slate-300 border-solid p-2 text-xs w-full mb-3 focus:border-slate-400 focus:outline-none rounded"
-              type="password"
-              placeholder="Password"
-              {...register("password", {
-                required: true,
-                minLength: 8,
-              })}
-            />
+            <div>
+              <input
+                className="border-[1px] border-slate-300 border-solid p-2 text-xs w-full mb-3 focus:border-slate-400 focus:outline-none rounded"
+                type="text"
+                placeholder="Username or email"
+                {...register("usernameOrEmail", {
+                  required: true,
+                  minLength: 4,
+                })}
+              />
+            </div>
+            <div className="relative">
+              <input
+                className="border-[1px] border-slate-300 border-solid p-2 text-xs w-full mb-3 focus:border-slate-400 focus:outline-none rounded"
+                type={passwordHidden ? "password" : "text"}
+                placeholder="Password"
+                {...register("password", {
+                  required: true,
+                  minLength: 8,
+                })}
+              />
+              <button
+                className={`absolute right-2 top-2 text-sm font-bold hover:text-gray-400 ${
+                  !password || password.length === 0 ? "hidden" : ""
+                }`}
+                onClick={() => setPasswordHidden((prev) => !prev)}
+              >
+                {passwordHidden ? "Show" : "Hide"}
+              </button>
+            </div>
             <button
               className="bg-blue-500 disabled:opacity-70 w-full text-white p-2 rounded-lg text-sm font-bold enabled:hover:bg-blue-600"
               type="submit"
--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -6,6 +6,7 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import getUrqlClient from "@/utils/getUrqlClient";
 import { graphql } from "@/generated";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const pacifico = Pacifico({ weight: "400", subsets: ["latin"] });
 
@@ -20,6 +21,8 @@ const Login = () => {
   const router = useRouter();
   const client = getUrqlClient();
 
+  const [passwordHidden, setPasswordHidden] = useState(true);
+
   const mutation = graphql(`
     mutation Register($data: RegisterInput!) {
       register(data: $data) {
@@ -30,11 +33,14 @@ const Login = () => {
 
   const {
     register,
+    watch,
     handleSubmit,
     formState: { isValid },
   } = useForm<Inputs>({
     mode: "onChange",
   });
+
+  const password = watch("password");
 
   const onSubmit: SubmitHandler<Inputs> = async (inputs) => {
     const { error } = await client.mutation(mutation, {
@@ -53,39 +59,55 @@ const Login = () => {
             Sign up to see photos and videos from your friends.
           </p>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <input
-              className="block border-[1px] border-slate-300 border-solid p-2 text-xs w-full mb-3 focus:border-slate-400 focus:outline-none rounded"
-              type="email"
-              placeholder="Email"
-              {...register("email", {
-                required: true,
-              })}
-            />
-            <input
-              className="block border-[1px] border-slate-300 border-solid p-2 text-xs w-full mb-3 focus:border-slate-400 focus:outline-none rounded"
-              type="text"
-              placeholder="Full Name"
-              {...register("fullName", {
-                required: true,
-              })}
-            />
-            <input
-              className="block border-[1px] border-slate-300 border-solid p-2 text-xs w-full mb-3 focus:border-slate-400 focus:outline-none rounded"
-              type="text"
-              placeholder="Username"
-              {...register("username", {
-                required: true,
-              })}
-            />
-            <input
-              className="block border-[1px] border-slate-300 border-solid p-2 text-xs w-full mb-3 focus:border-slate-400 focus:outline-none rounded"
-              type="password"
-              placeholder="Password"
-              {...register("password", {
-                required: true,
-                minLength: 8,
-              })}
-            />
+            <div>
+              <input
+                className="border-[1px] border-slate-300 border-solid p-2 text-xs w-full mb-3 focus:border-slate-400 focus:outline-none rounded"
+                type="email"
+                placeholder="Email"
+                {...register("email", {
+                  required: true,
+                })}
+              />
+            </div>
+            <div>
+              <input
+                className="border-[1px] border-slate-300 border-solid p-2 text-xs w-full mb-3 focus:border-slate-400 focus:outline-none rounded"
+                type="text"
+                placeholder="Full Name"
+                {...register("fullName", {
+                  required: true,
+                })}
+              />
+            </div>
+            <div>
+              <input
+                className="border-[1px] border-slate-300 border-solid p-2 text-xs w-full mb-3 focus:border-slate-400 focus:outline-none rounded"
+                type="text"
+                placeholder="Username"
+                {...register("username", {
+                  required: true,
+                })}
+              />
+            </div>
+            <div className="relative">
+              <input
+                className="border-[1px] border-slate-300 border-solid p-2 text-xs w-full mb-3 focus:border-slate-400 focus:outline-none rounded"
+                type={passwordHidden ? "password" : "text"}
+                placeholder="Password"
+                {...register("password", {
+                  required: true,
+                  minLength: 8,
+                })}
+              />
+              <button
+                className={`absolute right-2 top-2 text-sm font-bold hover:text-gray-400 ${
+                  !password || password.length === 0 ? "hidden" : ""
+                }`}
+                onClick={() => setPasswordHidden((prev) => !prev)}
+              >
+                {passwordHidden ? "Show" : "Hide"}
+              </button>
+            </div>
             <button
               className="bg-blue-500 disabled:opacity-70 w-full text-white p-2 rounded-lg text-sm font-bold enabled:hover:bg-blue-600"
               type="submit"
